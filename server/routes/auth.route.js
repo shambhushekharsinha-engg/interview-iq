9770086913a4e279fe91a8b902e5929e0e27353d
shambhushekharsinha-engg <shambhushekharsinha@gmail.com>
2026-07-19 13:19:24 +0530

    refactor(router): format route mappings and insert trailing semicolons closes #181

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -4,8 +4,8 @@ import { googleAuth, logOut } from "../controllers/auth.controller.js"
 const authRouter = express.Router()
 
 
-authRouter.post("/google",googleAuth)
-authRouter.get("/logout",logOut)
+authRouter.post("/google", googleAuth);
+authRouter.get("/logout", logOut);
 
 
 export default authRouter
