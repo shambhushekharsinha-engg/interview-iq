37c631ae148fc0500c431391385c22debf059f48
shambhushekharsinha-engg <shambhushekharsinha@gmail.com>
2026-07-19 12:47:00 +0530

    docs: add JSDoc comments to token generation utility closes #160

--- server/config/token.js
+++ server/config/token.js
@@ -1,5 +1,10 @@
 import jwt from "jsonwebtoken"
 
+/**
+ * Generates a signed JWT session token for the specified user ID.
+ * @param {string} userId - Mongoose user object ID
+ * @returns {Promise<string>} Signed JSON Web Token string
+ */
 const genToken = async (userId) => {
     try {
         const token = jwt.sign({userId} , process.env.JWT_SECRET , {expiresIn:"7d"})
