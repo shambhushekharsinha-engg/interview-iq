1cb0d755dd8bb31d00b477b6ad78e1661901d217
shambhushekharsinha-engg <shambhushekharsinha@gmail.com>
2026-07-19 12:48:19 +0530

    refactor: adjust fallback port configuration in server index closes #176

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use("/api/user", userRouter)
 app.use("/api/interview" , interviewRouter)
 app.use("/api/payment" , paymentRouter)
 
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 8000
 app.listen(PORT , ()=>{
     console.log(`Server running on port ${PORT}`)
     connectDb()
